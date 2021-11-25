c9dff12f206ada85be88e97c51b342772716d145
mayex-gif <alcaldealejo53@gmail.com>
2021-11-25 08:21:51 -0300

    responsive-btn

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,19 +2,9 @@
   <v-app>
     <v-hover v-slot="{ hover }" open-delay="15" close-delay="15">
       <v-app-bar :elevation="hover ? 20 : 5" :class="{ 'on-hover':hover }" clipped-left clipped-right app>
-        <v-toolbar-title class="mx-16" style="letter-spacing: 1px; font-family: 'Teko', sans-serif;">
+        <v-toolbar-title class="ml-0" style="letter-spacing: 1px; font-family: 'Teko', sans-serif; text-align: center;">
           <strong>Alcalde Alejo Emanuel</strong>
         </v-toolbar-title>
-        <v-hover v-slot="{ hover }" open-delay="15" close-delay="15">
-          <v-btn :elevation="hover ? 5 : 2" :class="{ 'on-hover':hover }" color="blue" outlined icon style="margin-left:58%;" href="https://www.linkedin.com/in/alejo-alcalde-5a0314215/">
-            <v-icon>mdi-linkedin</v-icon>
-          </v-btn>
-        </v-hover>
-        <v-hover v-slot="{ hover }" open-delay="15" close-delay="15">
-          <v-btn :elevation="hover ? 5 : 2" :class="{ 'on-hover':hover }" color="" outlined icon href="https://github.com/mayex-gif" style="margin-left: 2%;">
-            <v-icon>mdi-github</v-icon>
-          </v-btn>
-        </v-hover>
       </v-app-bar>
       </v-hover>
     <v-main>
@@ -24,18 +14,18 @@
     <v-col justify="center" no-gutters>
       <v-col class="py-4 text-center">
         <v-row class="flex-column">
-          <strong style="font-family: 'Teko', sans-serif; letter-spacing: 5px;">CONTACTO</strong>
-          <v-btn v-for="(item, i) in botones" :key="i" :href="item.to" text rounded class="my-2" style="width: 100%;">
+          <strong style="font-family: 'Teko', sans-serif; letter-spacing: 5px;" class="mt-5">CONTACTO</strong>
+          <v-btn outlined v-for="(item, i) in botones" :key="i" :href="item.to" text rounded class="my-2" style="width: 84%; margin-left: 8%; background: #fff;">
             <v-icon :color="item.color">{{ item.icon }}</v-icon>
             <p class="ma-2">{{ item.text }}</p>
           </v-btn>
         </v-row>
       </v-col>
-        <v-divider class="my-2 mx-16"></v-divider>
-        <v-card-text class="mb-1 text-center white--text">
-          Not full responsive website
+        <v-divider class="pb-1 mt-2"></v-divider>
+        <v-card-text class=" text-center white--text">
+          Full responsive website
         </v-card-text>
-        <v-divider class="mY-2"></v-divider>
+        <v-divider class="my-2"></v-divider>
         <v-card-text class="py-1  text-center white--text">
           <v-icon color="white">mdi-copyright</v-icon>
           Copyright {{ new Date().getFullYear() }} — Alcalde Alejo Emanuel— <strong>Vuetify</strong>
@@ -75,6 +65,18 @@ export default {
           to: 'mailto:alcaldealejo53@gmail.com',
           text: 'alcaldealejo53@gmail.com',
         },
+        {
+          icon: 'mdi-github',
+          color: '',
+          to: 'https://github.com/mayex-gif',
+          text: 'mayex-gif',
+        },
+        {
+          icon: 'mdi-linkedin',
+          color: '#0e76a8',
+          to: 'https://www.linkedin.com/in/alejo-alcalde-5a0314215/',
+          text: 'Alejo Alcalde',
+        },  
       ],
     }
   }
